f0b1e11b3b4e80df5dadfd78a42a8b55a96f4021
Walze <catufuzgu@gmail.com>
2017-12-19 18:45:50 -0200

    Removed Comments

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// for testing purposes
-// const hardCoded = require('./testIDs.hardcoded.temp')
-
 const
   seconds2String = require('./helpers/seconds2String'),
   playlist = require('./YTUrlGen'),
@@ -15,49 +12,3 @@ fetcher.get(playlist.url, () => {
   console.log('\n')
   console.log(`Average => ${seconds2String(fetcher.totalDuration / fetcher.totalVideos)}`)
 });
-
-/*
-get(playlist.url, () => {
-  const chunks = chunk(IDs, 50)
-
-  let totalDuration = 0
-
-  let counter = 1;
-
-  (function () {
-    return new Promise(next => {
-      // Each chunk has 50 IDs
-      chunks.map((chunk, ichunks) => {
-        axios.get(playlist.getVideo(chunk.join(',')))
-          .then(res => {
-            // Gets 50 vids in response
-
-            res.data.items.map((video, ivideos, videos) => {
-              totalDuration += Number(duration2secs(video.contentDetails.duration))
-              counter++
-
-              if (counter >= IDs.length) next()
-
-            })
-          }).catch(err => console.log(err))
-      })
-    })
-  })().then(() => {
-
-    console.log('\n')
-    console.log(`Total Duration => ${seconds2String(totalDuration)}`)
-    console.log('\n')
-    console.log(`Number of Videos => ${IDs.length}`)
-    console.log('\n')
-    console.log(`Average => ${seconds2String(totalDuration / IDs.length)}`)
-
-  })
-})
-
-
-/*
-// 
-var date = new Date(null);
-date.setSeconds(SECONDS); //specify value for SECONDS here
-var result = date.toISOString().substr(11, 8);
-*/
